23b3b8e8414a1125ed8fe2942393e8076d96e210
Martin Tribo <martin.tribo@gmail.com>
2017-08-06 17:32:23 -0500

    First demo version

--- index.js
+++ index.js
@@ -1,70 +1,91 @@
 var request = require('request');
 
-var defaultConfig = {
-  masterServer: 'http://slapp.io',
-  name: process.env.PROJECT_DOMAIN ? process.env.PROJECT_DOMAIN + '.glitch.me' : '',
-  url: process.env.PROJECT_DOMAIN ? 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me' : '',
-  users: 0,
-  serviceId: null
-};
+class Service {
+  constructor(config) {
+    this.masterServer = 'http://slapp.io';
+    this.name = process.env.PROJECT_DOMAIN ? process.env.PROJECT_DOMAIN + '.glitch.me' : '';
+    this.url = process.env.PROJECT_DOMAIN ? 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me' : '';
+    this.users = 0;
+    this.serviceId = null;
+    this.heartBeatActive = false;
+    this.interval = null;
 
-function postMasterServer(config) {
-  request.post(
-    config.masterServer + '/api/services',
-    {
-      json: {
-        name: config.name,
-        url: config.url,
-        users: config.users
-      }
-    }, function(error, response, body) {
-      config.serviceId = body;
-    }
-  );
-}
+    Object.assign(this, config);
 
-function pingMasterServer(config) {
-  if (config.serviceId != null) {
-    request.put(
-      config.masterServer + '/api/services/' + config.serviceId,
+    request.post(
+      this.masterServer + '/api/services',
       {
         json: {
-          users: config.users
+          name: this.name,
+          url: this.url,
+          users: this.users
         }
-      }, function(error, response, body) {
-        // Nothing for now
+      }, (error, response, body) => {
+        this.serviceId = body;
       }
     );
+  }
+
+  startHeartbeat() {
+    if (this.interval != null) {
+      this.interval = setInterval(() => {
+        this.ping(finalConfig);
+      }, 1000 * 60);
+    }
+  }
+  stopHeartbeat() {
+    if (this.interval != null) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+
+  increaseUserCount() {
+    this.users++;
+    this.ping();
+  }
+  decreaseUserCount() {
+    this.users--;
+    this.ping();
+  }
+
+  ping() {
+    if (this.serviceId != null) {
+      request.put(
+        this.masterServer + '/api/services/' + this.serviceId,
+        {
+          json: {
+            users: this.users
+          }
+        }, (error, response, body) => {
+          // Nothing for now
+        }
+      );
+    }
   }
 }
 
 module.exports = {
+  // TODO: Hack around easyrtc's single listener per event
   easyrtc: function (easyrtc, config) {
-    var finalConfig = Object.assign({}, defaultConfig, config);
-    postMasterServer(finalConfig);
+    var service = new Service(config);
 
     easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-      console.log('increase users');
-      finalConfig.users++;
-      pingMasterServer(finalConfig);
+      service.users++;
+      service.ping();
     });
     easyrtc.events.on("roomLeave", function(connectionObj, roomName, roomParameter, callback) {
-      finalConfig.users--;
-      console.log('decrease');
-      pingMasterServer(finalConfig);
+      service.users--;
+      service.ping();
     });
 
-    setInterval(function () {
-      pingMasterServer(finalConfig);
-    }, 1000 * 60);
+    service.startHeartbeat();
+    return service;
   },
 
-  noservice: function (config) {
-    var finalConfig = Object.assign({}, defaultConfig, config);
-    postMasterServer(finalConfig);
-
-    setInterval(function () {
-      pingMasterServer(finalConfig);
-    }, 1000 * 60);
+  simple: function (config) {
+    var service = new Service(config);
+    service.startHeartbeat();
+    return service;
   }
 };
